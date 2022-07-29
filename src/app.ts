86cf9768c19d665f0a875607f513a1206453a164
debora <debora.r.serra@gmail.com>
2022-07-29 14:46:58 -0300

    feat: add req 01

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import productRoute from './routes/product.route';
+import errorMiddleware from './middlewares/error.middleware';
 
 const app = express();
 
 app.use(express.json());
+app.use('/products', productRoute);
+
+app.use(errorMiddleware);
 
 export default app;
---
+++ src/errors/requiredError.ts
@@ -0,0 +1,8 @@
+export default class RequiredError extends Error {
+  code: number
+  constructor(message: string) {
+    super(message);
+    this.name = 'requiredError';
+    this.code = 400
+  }
+}
