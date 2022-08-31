0b6d7fcf5611f6c1b9ebfe818c67dff31fe2c941
debora <debora.r.serra@gmail.com>
2022-08-31 08:43:04 -0300

    feat: inicia frontend com tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mx-auto bg-green-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-4xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
+    </div>
+  );
+}
+export default App;
